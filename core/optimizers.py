3689da5404ad154177828243276b63c11b8ced06
siprok <57614998+siprok@users.noreply.github.com>
2022-04-01 02:09:03 +0300

    Completed the most primitive version of gradient descent

--- core/optimizers.py
+++ core/optimizers.py
@@ -9,3 +9,13 @@ class Optimizer(ABC):
     @abstractmethod
     def reboot(self):
         pass
+
+
+class GradDesc(Optimizer):
+    def __init__(self, learning_rate: np.float32):
+        self.lr = learning_rate
+    def optimize(self, weights_matrix: np.ndarray, error_der_matrix: np.ndarray):
+        assert np.array_equal(weights_matrix.shape, error_der_matrix.shape)
+        return weights_matrix - self.lr * error_der_matrix
+    def reboot(self):
+        pass
