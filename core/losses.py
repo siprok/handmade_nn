88e7de7b22efbd2e004f7a2426d24d7dd988ebff
siprok <57614998+siprok@users.noreply.github.com>
2022-04-01 01:58:55 +0300

    Completed crossentropy loss realization

--- core/losses.py
+++ core/losses.py
@@ -11,3 +11,15 @@ class Loss(ABC):
     @staticmethod
     def grad(outputs: np.ndarray, target: np.ndarray):
         pass
+
+
+class Crossentropy(Loss):
+    def calc(prediction: np.ndarray, target: np.ndarray) -> np.float32:
+        return - (target.flatten() * prediction.flatten()).sum() 
+
+    def grad(outputs: np.ndarray, target: np.ndarray) -> np.nadrray:
+        """outputs (1, n_classes) target (1, n_classes)"""
+        ind = np.argwhere(target.flatten() > 0)
+        error = np.zeros((target.size(), 1))
+        error[ind] = 1 / outputs[ind]
+        return error
