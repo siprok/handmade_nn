f539ca62bc6eb347f643437ea2f4b8bb49c98230
siprok <57614998+siprok@users.noreply.github.com>
2022-04-01 01:19:46 +0300

    Fixed bug in Dense layer; completed MNISTDense model

--- core/layers.py
+++ core/layers.py
@@ -9,7 +9,7 @@ class Layer(ABC):
     def forward(self, inputs: np.ndarray) -> np.ndarray:
         pass
     @abstractmethod
-    def backward(self, error_grad: np.ndarray) -> np.ndarray:
+    def backward(self, inputs: np.ndarray, error_grad: np.ndarray) -> np.ndarray:
         pass
 
 
@@ -33,17 +33,18 @@ class Dense(Layer):
     def forward(self, inputs: np.ndarray) -> np.ndarray:
         """inputs: np.ndarray (1, n_input)"""
         assert len(inputs.shape) == 2 and inputs.shape[0] == 1
-        self.outputs = self.activation((inputs * self.weights).sum(axis=0))
-        return self.outputs
+        return self.activation((inputs * self.weights).sum(axis=0))
+        
 
-    def backward(self, error_grad: np.ndarray) -> np.ndarray:
+    def backward(self, inputs: np.ndarray, error_grad: np.ndarray) -> np.ndarray:
         """надо вернуть градиент ошибки по выходам предыдущего слоя и в текущем поменять веса"""
+        assert len(inputs.shape) == 2 and inputs.shape[0] == 1
         # Найдем градиент ошибки по входам функции активации
         grad_out_by_summator = self.activation.error_back_prop(self.outputs, error_grad)
         # Найдем градиент ошибки по выходам предыдущего слоя
         grad_by_prev_layer = self.weights.dot(grad_out_by_summator)
         # Найдем матрицу производных ошибки по весам нейронов текущего слоя
-        error_der_matrix = self.outputs.T.dot(error_grad.T)
+        error_der_matrix = inputs.T.dot(error_grad.T)
         # Произведем шаг оптимизации весов по найенным производным
         self.weights = self.optimizer(self.weights, error_der_matrix)
         # Вернем градиент ошибки по выходам предыдущего слоя
--- core/models.py
+++ core/models.py
@@ -1,34 +1,13 @@
 import numpy as np
-from typing import Callable, Sequence
+from copy import copy, deepcopy
+from typing import Sequence
 from abc import ABC, abstractmethod
+from .initializers import Initializer
+from .optimizers import Optimizer
+from .layers import Layer, Dense
+from .lossees import Loss
 
 
-class Initializer(ABC):
-    @abstractmethod
-    def initialize(self):
-        pass
-    
-class Optimizer(ABC):
-    @abstractmethod
-    def optimize(self):
-        pass
-    
-class Activation(ABC):
-    @abstractmethod
-    def calc(self):
-        pass
-    @abstractmethod
-    def grad(self):
-        pass
-    
-class Layer(ABC):
-    @abstractmethod
-    def forward(self):
-        pass
-    @abstractmethod
-    def back(self):
-        pass
-    
 class Model(ABC):
     @abstractmethod
     def fit(train_samples: np.ndarray,
@@ -37,6 +16,9 @@ class Model(ABC):
     @abstractmethod
     def predict(test_samples: np.ndarray) -> np.ndarray:
         pass
+
+
+class Classifier(Model):
     @abstractmethod
     def predict_proba(test_samples: np.ndarray) -> np.ndarray:
         pass
@@ -44,18 +26,51 @@ class Model(ABC):
     
 class MNISTDense(Model):
     def __init__(self,
-                 layers_sizes:Sequence[int],
-                 loss: Callable[[np.ndarray, np.ndarray], np.float32],
-                 initializer: Initializer,
-                 optimizer: Optimizer):
-        pass
+                 input_shape: int,
+                 layers_sizes: Sequence[int],
+                 initializers_classes: Sequence[type],
+                 activations_classes: Sequence[type],
+                 optimizer: Optimizer,
+                 loss: type,
+                 initializer: Initializer):
+        assert len(layers_sizes) == len(initializers_classes) == len(activations_classes)
+        assert issubclass(loss, Loss)
+        self.loss = loss
+        self.layers_sizes = list(copy(layers_sizes))
+        self.layers = []
+        for i, size, initializer, activator in enumerate(zip(layers_sizes,
+                                                            initializers_classes,
+                                                            activations_classes)):
+            self.layeres.append(
+                Dense(
+                    size=size,
+                    prev_size=layers_sizes[i-1] + 1 if i > 0 else input_shape,
+                    next_size=layers_sizes[i+1] if i + 1 < len(layers_sizes) else 0,
+                    initializers_class=initializer,
+                    activation_class=activator,
+                    optimizer=deepcopy(optimizer)
+                )
+            )
     
-    def fit(train_samples: np.ndarray,
-            train_labels: np.ndarray):
-        pass
-    
+    def fit(train_samples: np.ndarray, train_labels: np.ndarray):
+        losses = np.empty(len(train_labels), dtype=np.float32)
+        for i, (sample, target) in enumerate(zip(train_samples, train_labels)):
+            layers_inputs = np.empty((max(self.layers_sizes) + 1, len(self.layers) + 1), dtype=np.float32)
+            layers_inputs[self.layers_sizes, range(len(self.layers_sizes))] = 1  # значения входов для смещения
+            layers_inputs[:self.layers_sizes[0], 0] = sample.reshape((1,-1))
+            for j, (layer, cur_size, next_size) in enumerate(zip(self.layers, self.layers_sizes, self.layers_sizes[1:] + [self.layers_sizes[-1]] )): # распространение вперёд
+                layers_inputs[:next_size, j + 1] = layer.forward(layers_inputs[:cur_size, j])
+            output = layers_inputs[:self.layers_sizes[-1], -1]
+            losses[i] = self.loss.calc(output, target)
+            error_grad = self.loss.grad(output, target)
+            for j, (layer, inp_size) in enumerate(zip(self.layers[::-1], self.layers_sizes[-2::-1] + [self.layers_sizes[0]])): # распространение назад
+                inputs = layers_inputs[: inp_size + 1, -(j+2)]
+                error_grad = layer.backward(inputs, error_grad)
+        return losses
+
     def predict(test_samples: np.ndarray) -> np.ndarray:
-        pass
-    
-    def predict_proba(test_samples: np.ndarray) -> np.ndarray:
-        pass
+        for sample in enumerate(train_samples):
+            output = np.block([sample.reshape((1,-1)), 1])
+            for layer in self.layers: # распространение вперёд
+                output = layer.forward(np.block([output, 1]))
+        return output 
