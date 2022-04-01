bb24a62b112d0f94f7f93010a2bec0039d4cc9aa
siprok <siprokudin@gmail.com>
2022-04-01 12:14:47 +0300

    Added epoches to fit

--- core/models.py
+++ core/models.py
@@ -2,9 +2,8 @@ import numpy as np
 from copy import copy, deepcopy
 from typing import Sequence
 from abc import ABC, abstractmethod
-from .initializers import Initializer
 from .optimizers import Optimizer
-from .layers import Layer, Dense
+from .layers import Dense
 from .losses import Loss
 import pdb
 
@@ -53,20 +52,21 @@ class MNISTDense(Model):
                 )
             )
     
-    def fit(self, train_samples: np.ndarray, train_labels: np.ndarray):
-        losses = np.empty(len(train_labels), dtype=np.float32)
-        for i, (sample, target) in enumerate(zip(train_samples, train_labels)):
-            layers_inputs = np.empty((max(self.layers_sizes + [self.input_size]) + 1, len(self.layers) + 1), dtype=np.float32)
-            layers_inputs[self.layers_sizes, range(len(self.layers_sizes))] = 1  # значения входов для смещения
-            layers_inputs[:self.input_size, 0] = sample.reshape((1,-1))
-            for j, (layer, input_size, output_size) in enumerate(zip(self.layers, [self.input_size] + self.layers_sizes[:-1], self.layers_sizes )): # распространение вперёд
-                layers_inputs[:output_size, j + 1] = layer.forward(layers_inputs[:input_size+1, j].reshape((1,-1)))
-            output = layers_inputs[:self.layers_sizes[-1], -1]
-            losses[i] = self.loss.calc(output, target)
-            error_grad = self.loss.grad(output, target)
-            for j, (layer, inp_size) in enumerate(zip(self.layers[::-1], self.layers_sizes[-2::-1] + [self.input_size])): # распространение назад
-                inputs = layers_inputs[: inp_size + 1, -(j+2)].reshape((1,-1))
-                error_grad = layer.backward(inputs, error_grad)
+    def fit(self, epoch: int, train_samples: np.ndarray, train_labels: np.ndarray):
+        losses = np.empty((epoch, len(train_labels)), dtype=np.float32)
+        layers_inputs = np.empty((max(self.layers_sizes + [self.input_size]) + 1, len(self.layers) + 1), dtype=np.float32)
+        layers_inputs[self.layers_sizes, range(len(self.layers_sizes))] = 1  # значения входов для смещения
+        for e in range(epoch):
+            for i, (sample, target) in enumerate(zip(train_samples, train_labels)):    
+                layers_inputs[:self.input_size, 0] = sample.reshape((1,-1))
+                for j, (layer, input_size, output_size) in enumerate(zip(self.layers, [self.input_size] + self.layers_sizes[:-1], self.layers_sizes )): # распространение вперёд
+                    layers_inputs[:output_size, j + 1] = layer.forward(layers_inputs[:input_size+1, j].reshape((1,-1)))
+                output = layers_inputs[:self.layers_sizes[-1], -1]
+                losses[e, i] = self.loss.calc(output, target)
+                error_grad = self.loss.grad(output, target)
+                for j, (layer, inp_size) in enumerate(zip(self.layers[::-1], self.layers_sizes[-2::-1] + [self.input_size])): # распространение назад
+                    inputs = layers_inputs[: inp_size + 1, -(j+2)].reshape((1,-1))
+                    error_grad = layer.backward(inputs, error_grad)
         return losses
 
     def predict(self, test_samples: np.ndarray) -> np.ndarray:
